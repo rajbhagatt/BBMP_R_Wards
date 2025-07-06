092c0013ff42dfa4bd8ccb8288953873e014343f
rajbhagatt <rajbhagatt@gmail.com>
2025-07-06 09:18:32 +0530

    Add files via upload

--- script/mapper.js
+++ script/mapper.js
@@ -1,4 +1,30 @@
+
+	function highlightFeature(e) {
+	//When the users hover over the polygons, they are highlighted
+    var layer = e.target;
+    layer.setStyle({weight: 5,color: '#666',dashArray: '',fillOpacity: 0.7});
+	info.update(layer.feature.properties);
+	}
+
+	function resetHighlight(e) {
+	//When the users hover out of the polygon,the highlight is removed
+		Ward_M.resetStyle(e.target);
+		info.update();
+	}
+
+	function onEachWard(feature,layer) {
+	//Within the map for every feature showing labels
 	
+	
+	//What to do when user hovers over
+    layer.on({
+		
+        mouseover: highlightFeature,
+        mouseout: resetHighlight
+		
+    });
+	
+	}
 	
 	function style_Parli(feature) {
 	
@@ -19,7 +45,7 @@
 	    fillColor: 'white',
 		weight: 3,
         opacity: 1,
-        color: 'blue',
+        color: 'black',
         dashArray: '3',
         fillOpacity: 0
 		};
@@ -40,6 +66,20 @@
 		};
 	
 	}
+	
+	function style_Ward_Transparent(feature) {
+	
+	return {
+	    fillColor: 'white',
+		weight: 0,
+        opacity: 0,
+        color: 'white',
+        dashArray: '1',
+        fillOpacity: 0
+	
+		};
+	
+	}
 
 	function style_Scenario(feature) {
 	
@@ -50,6 +90,46 @@
         color: 'red',
         dashArray: '1',
         fillOpacity: 0
+		};
+	
+	}
+	
+	function getColours(Corp) {
+	var chosencolour = 'blue';
+	if (Corp=="East") 
+	{
+		chosencolour='cyan';
+	}
+	else if (Corp=="West") 
+	{
+		chosencolour='red';
+	}
+	else if (Corp=="North") 
+	{
+		chosencolour='green';
+	}
+	else if (Corp=="South") 
+	{
+		chosencolour='orange';
+	}
+	else if (Corp=="Central") 
+	{
+		chosencolour='pink';
+	}
+	
+	return chosencolour;
+	
+	}
+	
+	function style_Scenario_v2(feature) {
+	
+	return {
+	    fillColor: getColours(feature.properties['NewCorp']),
+		weight: 1,
+        opacity: 1,
+        color: 'grey',
+        dashArray: '1',
+        fillOpacity: 0.3
 		};
 	
 	}
@@ -69,6 +149,8 @@
 	
 	
 	
+	
+	
 	//Do classification based on the merged data
 	
 	var zoom=10;
@@ -83,15 +165,29 @@
 
 	var Parli_M=L.geoJson(ParliConst, {style: style_Parli});
 	var Ass_M=L.geoJson(AssConst, {style: style_Ass});
-	var Ward_M=L.geoJson(BBMPWards, {style: style_Ward});
+	var Ward_M=L.geoJson(BBMPWards, {style: style_Ward_Transparent});
+	var Ward_N=L.geoJson(BBMPWards, {style: style_Ward});
 	var BBMP_OLD_M=L.geoJson(bbmpold, {style: style_BBMPOLD});
-        var scmap=L.geoJson(BBMP51, {style: style_Scenario});
+    var scmap=L.geoJson(BBMP522, {style: style_Scenario});
 
 	
 	googlebg = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}',{    maxZoom: 20,    subdomains:['mt0','mt1','mt2','mt3']}).addTo(map);
 
+	info = L.control({position: "topright"});
 
+	info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'hover'); // create a div with a class "info"
+    this.update();
+    return this._div;
+	};
+
+	// method that we will use to update the control based on feature properties passed
+	info.update = function (props) { 	
+    this._div.innerHTML =  (props ? ' Ward Name: <b>' + props['Ward_Name'] + '</b><br />Ward Number: <b>'+ props['Ward_No']+ '</b><br />Assembly: <b>'+ props['Assembly_C']
+        : '');
+	};
 	
+	info.addTo(map);
 	
 	function bgswitch() {
 		map.removeLayer(googlebg);
@@ -131,25 +227,15 @@
 
 function scswitch() {
 		map.removeLayer(scmap);
-		if (document.getElementById('scbox').value=="51") 
+		if (document.getElementById('scbox').value=="500") 
   {
-      scmap=L.geoJson(BBMP51, {style: style_Scenario}).addTo(map);
+      scmap=L.geoJson(BBMPOpt2, {style: style_Scenario_v2}).addTo(map);
   } 
-  else if (document.getElementById('scbox').value=="511") 
-  {
-	scmap=L.geoJson(BBMP511, {style: style_Scenario}).addTo(map);
-
-  }
-		
-		else if (document.getElementById('scbox').value=="52") 
-		{
-			scmap=L.geoJson(BBMP52, {style: style_Scenario}).addTo(map);
-
-		}
+  
 		
 		else if (document.getElementById('scbox').value=="522") 
 		{
-			scmap=L.geoJson(BBMP522, {style: style_Scenario}).addTo(map);
+			scmap=L.geoJson(BBMP522, {style: style_Scenario_v2}).addTo(map);
 
 		}
 		drawbasic();
@@ -157,7 +243,11 @@ function scswitch() {
 
 	function drawbasic() {
 	map.removeLayer(Ward_M);
+	/*
 	Ward_M=L.geoJson(BBMPWards, {style: style_Ward,onEachFeature: function (feature, layer) {layer.bindPopup("<table  style='width:500px;height:100%;font-size:12px'><tr><td><b>Ward Name:</b> "+feature.properties.Ward_Name +"<br><br><b>Number:</b> "+feature.properties.Ward_No+"<br><br><b>Assembly:</b> "+feature.properties.Assembly_C+"<br><br><b>Parliament: </b>"+feature.properties.MP_Constit+"<br><br><b>Scenario 5.1:</b> "+feature.properties.Sc5_1+"<br><br><b>Scenario 5.1.1:</b> "+feature.properties.Sc5_1_1+"<br><br><b>Scenario 5.2:</b> "+feature.properties.Sc5_2+"<br><br><b>Scenario 5.2.2:</b> "+feature.properties.Sc5_2_2+"</td></tr></table>")}}).addTo(map);
+	
+	*/
+	Ward_M=L.geoJson(BBMPWards, {style: style_Ward_Transparent,onEachFeature: onEachWard}).addTo(map);
 	}
 
 
@@ -169,23 +259,28 @@ function scswitch() {
         map.removeLayer(layer);
 		}
     });
-    if ($("#parlibox").is(":checked"))
-  {
-Parli_M=L.geoJson(ParliConst, {style: style_Parli}).addTo(map);
-
-  } 
+    
   
   if ($('#assbox').is(":checked"))
   {
-
+	  map.removeLayer(Ass_M);
       Ass_M=L.geoJson(AssConst, {style: style_Ass}).addTo(map);
+	  
   } 
-  
-  
+  if ($('#wardbox').is(":checked"))
+  {
+	  map.removeLayer(Ward_N);
+	  Ward_N=L.geoJson(BBMPWards, {style: style_Ward}).addTo(map);
+      
+	  
+  } 
+  scswitch();
+  /*
   if ($('#bbmpoldbox').is(":checked") )
   {
       BBMP_OLD_M=L.geoJson(bbmpold, {style: style_BBMPOLD}).addTo(map);
   } 
+  */
   
   
   
